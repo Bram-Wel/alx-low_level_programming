603123bd161492b9b44bcbc7c475a865744e9b8c
Bram-Wel <welbram@gmail.com>
2022-03-15 14:44:19 -0700

    Compute last digit.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Last digit.
+ * @n: Integer parameter.
+ *
+ * Description: Obtain Last digit of number n.
+ * Return: last digit $lstdgt.
+ */
+int print_last_digit(int n)
+{
+	int lstdgt = (n % 10);
+	return (lstdgt);
+}
