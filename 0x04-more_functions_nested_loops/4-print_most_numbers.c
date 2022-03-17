b066d310d59d066875898d0a30dacbc213393cfa
Bram-Wel <welbram@gmail.com>
2022-03-17 12:39:03 -0700

    Compare using ascii standard.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - Print no.s.
- * 
+ *
  * Description: Print no.s 0-9 leaving out 2 & 4..
  */
 void print_most_numbers(void)
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 	i = 48;
 	while (i <= 57)
 	{
-		if (i == 2 || i == 4)
+		if (i == 50 || i ==52)
 			i++;
 
 		_putchar(i);
