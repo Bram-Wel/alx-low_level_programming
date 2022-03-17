4271c71d042bd7aea54b4fe26bb8142838677808
Bram-Wel <welbram@gmail.com>
2022-03-17 12:30:48 -0700

    Add Function description.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Print no.s.
- *
+ * print_most_numbers - Print no.s.
+ * 
  * Description: Print no.s 0-9 leaving out 2 & 4..
  */
 void print_most_numbers(void)
