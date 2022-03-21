acc26a349eff306b4318777f7532777cf68b6666
Bram-Wel <welbram@gmail.com>
2022-03-21 17:58:17 +0000

    Swap integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * swap_int - Swap values of 2 integers.
+  *@a: Pointer to the 1st integer value.
+  *@b: Pointer to the 2nd integer value.
+  */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	a* = *b;
+	*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 void reset_to_98(int *n);
-void print_alphabet_x10(void);
+void swap_int(int *a, int *b);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
