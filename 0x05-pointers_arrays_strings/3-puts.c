222c31aa9e8eec6e2a373f1c97029d3837969e0e
Bram-Wel <welbram@gmail.com>
2022-03-21 19:03:50 +0000

    Function to write to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _puts - Print string to stdout
+  * @str: Pointer to String parameter to print.
+  */
+void _puts(char *str)
+{
+	char *buffer = *str;
+	write(1, *buffer, _strlen(*buffer));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-int _isdigit(int c);
+void _puts(char *str);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
