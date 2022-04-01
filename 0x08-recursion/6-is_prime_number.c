b8edec5ad2aa5f2c2f41431f88cf097821b8fa5b
Bram-Wel <welbram@gmail.com>
2022-04-01 11:08:37 +0000

    Betty function documentation guideline Compliance.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int is_prime(unsigned int n, int i);
+
 /**
   * is_prime_number - Checks that an input integer is prime.
   * @n: The input integer.
@@ -7,20 +9,19 @@
   * Description: Calls the support function is prime.
   * Return: 1 if prime, 0 if otherwise.
   */
-int is_prime(unsigned int n, int i); /* Declaration for support function.*/
-
 int is_prime_number(int n)
 {
-	return is_prime(n, 2);
+	return (is_prime(n, 2));
 }
+
 /**
   * is_prime - Checks if an integer is prime.
   * @n: The integer argument.
   * @i: Iteration variable.
   *
   * Description: As the variable n is constant, recursively parsing it to
-  * function will cause stack overflow, thus requiring a support function taking 2
-  * varibles where one can change.
+  * function will cause stack overflow, thus requiring a support function
+  * taking 2 varibles where one can change.
   * Return: 1 if prime, 0 if not.
   */
 int is_prime(unsigned int n, int i)
@@ -34,5 +35,5 @@ int is_prime(unsigned int n, int i)
 	if (n % i == 0)
 		return (0); /* ... it is indivisible by any other number less than n.*/
 
-	return is_prime(n, i + 1); /* Increment i with every recursive call*/
+	return (is_prime(n, i + 1)); /* Increment i with every recursive call*/
 }
