fce741c1116fd3e854f4c10f0dd5e1c10b9d7cbf
Bram-Wel <welbram@gmail.com>
2022-04-02 15:44:08 +0000

    Print 0 if negative.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,8 +26,10 @@ int main(int argc, char **argv)
 			ptr  = *(argv + i);
 
 			if (atoi(ptr) < 0)
+			{
+				printf("%d\n", 0);
 				return (0);
-
+			}
 			if (*ptr < 48 || *ptr > 57)
 			{
 				printf("Error\n");
