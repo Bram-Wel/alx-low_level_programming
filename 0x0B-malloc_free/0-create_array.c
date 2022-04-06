e357f69aac165d09a6bd9e0fb72e532c383c6358
Bram-Wel <welbram@gmail.com>
2022-04-06 14:41:43 +0000

    Request exact memory size for array.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
-	array = malloc(size * sizeof(char) + 1);
+	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
 		return (0);
