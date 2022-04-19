e3d15703a5b91cab1e43777365f1e7b2b54c9986
Bram-Wel <welbram@gmail.com>
2022-04-19 10:44:29 +0000

    Define new structure dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+  * struct dog - A structure holding the name, owner & age of a dog.
+  * @name: Dog's name.
+  * @owner: Dog's owner.
+  * @age: Dogs's age.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* ifndef DOG_H*/
