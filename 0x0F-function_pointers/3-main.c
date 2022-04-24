336f897bd077300a594ff352a8cdad07240a4628
Bram-Wel <welbram@gmail.com>
2022-04-24 19:05:18 +0000

    Perform simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(c) == NULL || *(c + 1) != '\0')
+	if (*c == '.' || get_op_func(c) == NULL || *(c + 1) != '\0')
 	{
 		printf("Error\n");
 		exit(99);
