9e973602157b8c3227baa8feafcb33815c4b37d2
Bram-Wel <welbram@gmail.com>
2022-04-24 19:16:29 +0000

    Handle NULL parameters.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array = NULL || action == NULL)
+		return;
+
 	for (i = 0; i < (unsigned int)size; i++)
 		action(*(array + i));
 }
