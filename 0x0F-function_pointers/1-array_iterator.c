c1d8cfa300132045d828646d5231644c3056be78
Bram-Wel <welbram@gmail.com>
2022-04-24 14:46:14 +0000

    Parsing function as parameter.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/*#include <stddef.h>*/
+
+/**
+  * array_iterator - Iterates through an integer array.
+  * @array: The iterated array.
+  * @size: Size of the array.
+  * @action: Pointer to function called in each itearation.
+  *
+  * Description: This function calls a function action
+  * as it iterates through an integer array operand of size @size.
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < (unsigned int)size; i++)
+		action(*(array + i));
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,10 +2,13 @@
 
 #define FUNCTION_POINTERS_H
 
-void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
 
+void print_name(char *name, void (*f)(char *));
 void f(char *);
 
 int _putchar(char c);
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* ifndef FUNCTION_POINTERS_H */
