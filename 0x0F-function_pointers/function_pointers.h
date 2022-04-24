06164d3dc96e846a9df669b25e53e160457ab437
Bram-Wel <welbram@gmail.com>
2022-04-24 16:04:26 +0000

    Search for an integer.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -11,4 +11,6 @@ int _putchar(char c);
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* ifndef FUNCTION_POINTERS_H */
