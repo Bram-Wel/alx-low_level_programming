be65c93387608cf227402593ba81de093b359ac2
Bram-Wel <welbram@gmail.com>
2022-04-25 18:11:34 +0000

    Handle 0.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		separator = "";
 	if (n == 0)
-		return (0);
+		return;
 
 	va_start(argptr, n);
 	i = 0;
