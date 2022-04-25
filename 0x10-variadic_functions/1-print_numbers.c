bcbbc12b76ff86169f59b30b2c3c671fe5562eba
Bram-Wel <welbram@gmail.com>
2022-04-25 18:07:46 +0000

    Handle 0.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-  * This function prints numbers.
+  * print_numbers - This function prints numbers.
   * @separator: String seperating printed no.s.
   * @n: No. of integers passed to the function.
   */
@@ -13,12 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		separator = "";
+	if (n == 0)
+		return;
 
 	va_start(argptr, n);
 	i = 0;
 	while (i < (n - 1))
 	{
-		printf("%d%s", va_arg(argptr, int),separator);
+		printf("%d%s", va_arg(argptr, int), separator);
 		i++;
 	}
 
