ece666dbb49cb1cee539c27ac6cb41e9f85c3bf8
Bram-Wel <welbram@gmail.com>
2022-04-26 20:16:15 +0000

    Make a print function.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,4 +11,20 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/**
+  * struct fmt - A format structure.
+  * @s: The Format specifier.
+  * @f: Pointer to a matching function.
+  */
+typedef struct fmt{
+	char * s;
+	void (*f)(va_list argptr);
+} fmt;
+
+void print_all(const char * const format, ...);
+void print_char(va_list argptr);
+void print_int(va_list argptr);
+void print_float(va_list argptr);
+void print_string(va_list argptr);
+
 #endif /* ifndef VARIADIC_FUNCTIONS_H*/
