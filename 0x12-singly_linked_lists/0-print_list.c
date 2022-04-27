9146c28bb09918a2b9750c5681bb8629b03d3298
Bram-Wel <welbram@gmail.com>
2022-04-27 22:33:42 +0000

    Handle null.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,6 +11,9 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
+	if (!h)
+		return (nodes);
+
 	while (h->next != 0)
 	{
 		if (h->str != 0)
@@ -22,15 +25,13 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 
-	if (h != 0)
-	{
-		if (h->str != 0)
-			printf("[%d] %s\n", h->len, h->str);
-		else
-			printf("[%d] %p\n", 0, h->str);
+	if (h->str != 0)
+		printf("[%d] %s\n", h->len, h->str);
+	else
+		printf("[%d] %p\n", 0, h->str);
 
-		nodes++;
-		h = h->next;
-	}
+	nodes++;
+	h = h->next;
+
 	return (nodes);
 }
