e5ab205be617a0ffd4c809ad0f82be06961ab44d
Bram-Wel <welbram@gmail.com>
2022-05-05 19:41:04 +0000

    File Copy program.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,4 +13,11 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/*cp - Program*/
+/*------------------*/
+void error100(int fd);
+void error99(char *filename);
+void error98(char *filename);
+size_t subs_read_write(int fd_r, char *buff, char *filename1, char *filename2);
+
 #endif /* #ifndef MAIN_H*/
