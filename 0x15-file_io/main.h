a6632bfedd0b765cf652cafd15b81c7f90ec0989
Bram-Wel <welbram@gmail.com>
2022-05-07 19:54:31 +0000

    Program to read elf files.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,4 +20,19 @@ void error99(char *filename);
 void error98(char *filename);
 size_t subs_read_write(int fd_r, char *buff, char *filename1, char *filename2);
 
+/*elf reader program.*/
+/*----------------------*/
+void cant_open(char *filename);
+void cant_read(char *filename);
+void cant_close(int fd);
+void check_elf(char *file, char *buff);
+void print_magic(char *buff);
+void print_class(char *buff);
+void print_data(char *buff);
+void print_version(char *buff);
+void print_os_abi(char *buff);
+void print_abi_v(char *buff);
+void print_f_type(char *buff);
+void print_address(char *buff);
+
 #endif /* #ifndef MAIN_H*/
