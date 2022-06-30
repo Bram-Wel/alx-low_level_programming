09b4577799e9259f06cb10748bd202a61bbecff6
Bram-Wel <welbram@gmail.com>
2022-06-30 16:45:22 +0000

    Doubly linked list.

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - Finds node at index.
+  * @index: Index of node, starting 0.
+  *
+  * Return: Node at index OR NULL if node doesn't exist.
+  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i <= index && head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+
+	return (0);
+}
