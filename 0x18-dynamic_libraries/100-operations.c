37afa533152b7e5f8adbde9420e19bb863825048
Bram-Wel <welbram@gmail.com>
2022-07-01 15:39:09 +0000

    Shared objects.

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+/**
+  * add - Adds 2 integers.
+  * @a: Operand 1.
+  * @b: Operand 2.
+  *
+  * Return: Sum of a & b.
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * sub - Subtracts 2 integers.
+  * @a: Operand 1.
+  * @b: Operand 2.
+  *
+  * Return: Difference of a & b.
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * mul - Multiplies 2 integers.
+  * @a: Operand 1.
+  * @b: Operand 2.
+  *
+  * Return: Product of a & b.
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * div - Divides 2 integers.
+  * @a: Operand 1.
+  * @b: Operand 2.
+  *
+  * Return: Quotient of a & b.
+  */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * add - Modulus of 2 integers.
+  * @a: Operand 1.
+  * @b: Operand 2.
+  *
+  * Return: Mpdulus of a & b.
+  */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,7 +2,7 @@
 
 #define MAIN_H
 
-#include <stdlib.h>
+//#include <stdlib.h>
 
 int _putchar(char c);
 int _putchar(char c);
@@ -25,5 +25,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 #endif /*MAIN_H*/
