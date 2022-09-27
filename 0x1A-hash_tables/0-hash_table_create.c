7ab6a13ed40b520bdf3fb7e0c63e081cd4a3f33f
Bram-Wel <welbram@gmail.com>
2022-09-27 20:00:02 +0000

    Inserting to hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
   */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (!ht)
@@ -16,5 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!ht->array)
 		return (0);
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = 0;
+
 	return (ht);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/*#include <stdio.h>*/
+
+/**
+  * hash_table_set - Adds an element into a hash table
+  *
+  * @ht: Hash table into which to insert the element
+  * @key: The key.
+  * Used to compute the index for insertion
+  * @value: Value that is associated with key
+  * Return: 1 on success, 0 on failure
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int size = sizeof(*ht);
+	unsigned long int idx;
+	hash_node_t *nd = malloc(sizeof(hash_node_t));
+
+	if (!key || !nd)
+		return (0);
+
+	nd->key = strdup(key);
+	nd->value = strdup(value);
+	nd->next = 0;
+	idx = key_index((unsigned char *)key, size);
+	if (!ht->array[idx])
+	{
+		ht->array[idx] = nd;
+	}
+	else
+	{
+		nd->next = ht->array[idx];
+		ht->array[idx] = nd;
+		/*printf("Collision\n");*/
+	}
+	return (1);
+}
