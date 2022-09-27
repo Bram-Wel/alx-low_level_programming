d82a01d6c28db802233d1937081073d61dd1d23f
Bram-Wel <welbram@gmail.com>
2022-09-27 20:52:16 +0000

    Searching hash table.

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_get - retrieves a value associated with a key
+  *
+  * @ht: Hash table to search
+  * @key: key to find
+  * Return: value or null if no key is found
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+
+	if (!ht || !key)
+		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx])
+		return (ht->array[idx]->value);
+	return (0);
+}
