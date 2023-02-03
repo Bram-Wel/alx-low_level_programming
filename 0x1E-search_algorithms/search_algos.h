84daccc2f935b33378a9e92988fedf8194aa6be4
Bram-Wel <welbram@gmail.com>
2023-02-03 18:45:45 +0300

    Advanced Binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ void print(int *array, int left, int right);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 #endif /*SEARCH_ALGOS_H*/
 
