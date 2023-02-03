41ddaaceaefdf83f754c8eb66c8e0dd6f3ea22f1
Bram-Wel <welbram@gmail.com>
2023-02-03 19:05:26 +0300

    Push list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,6 +13,23 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 void print_array(int *array, int left, int right);
 int binary_search_new(int *array, int left, int right, int value);
 int exponential_search(int *array, size_t size, int value);
+
+/**
+ * struct listint_s - singly
+ *
+ * @n: Integer
+ * @index: Index of 
+ * @next: Pointer 
+ *
+ * Description: singly  
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 listint_t *jump_it(listint_t *start, int end);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif /*SEARCH_ALGOS_H*/
