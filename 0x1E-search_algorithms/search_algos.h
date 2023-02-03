3e74a11dfd103d7f86cd7a9de39cfc9b07661861
Bram-Wel <welbram@gmail.com>
2023-02-03 19:07:25 +0300

    Push list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+#include <math.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print(int *array, int left, int right);
