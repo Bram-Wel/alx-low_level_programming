a31398b725da65ece642f3ada350b33f30887721
Bram-Wel <welbram@gmail.com>
2023-02-03 18:57:36 +0300

    Exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
+void print_array(int *array, int left, int right);
+int binary_search_new(int *array, int left, int right, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif /*SEARCH_ALGOS_H*/
 
