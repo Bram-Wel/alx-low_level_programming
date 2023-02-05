fae7b6af3aff8243c0e5be002b46685869dcd414
Bram-Wel <welbram@gmail.com>
2023-02-05 16:43:07 +0300

    Island perimeter: Additional qualifiers

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,19 +22,25 @@ def island_perimeter(grid):
     """
     perimeter = 0
 
+    if not isinstance(grid, list) or not isinstance(grid[0], list):
+        return 0
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             try:
-                if grid[i][j] == 1:
-                    if grid[i][j + 1] == 0:
+                if grid[i][j] == 0:
+                    if grid[i][j + 1] == 1:
                         perimeter += 1
-                    if grid[i][j - 1] == 0:
+                    if grid[i][j - 1] == 1:
                         perimeter += 1
-                    if grid[i - 1][j] == 0:
+                    if grid[i - 1][j] == 1:
                         perimeter += 1
-                    if grid[i + 1][j] == 0:
+                    if grid[i + 1][j] == 1:
                         perimeter += 1
             except IndexError as e:
                 # print(e)
                 pass
+
+    if perimeter == 0:
+        perimeter = len(grid) * len(grid[1])
     return perimeter
